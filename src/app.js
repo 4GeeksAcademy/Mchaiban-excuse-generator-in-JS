4fda32e2496f000e6c899c88f0704038677cc24d
Mchaiban <marianafeels@gmail.com>
2025-04-01 21:15:29 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The panda', 'My mom', 'The musician', 'My friend'];
+let action = ['wrote', 'crushed', 'hacked', 'broke'];
+let what = ['my homework', 'my phone', 'the car', 'my dreams'];
+let when = ['before the earthquake', 'when I was contemplating life', 'while I was eating an ice-cream', 'during my lunch', 'while I was watching YouTube'];
+
+function getRandomNumber (min,max) {
+   return Math.floor(Math.random() * (max-min) + min)
+}
+
+function randomExcuse (anyArray) {
+  let max = anyArray.length;
+  let min = 0;
+  let random = getRandomNumber (min,max);
+  return anyArray [random];
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector ('#excuse') .innerHTML = randomExcuse(who) + " " + randomExcuse(action) + " " + randomExcuse(what) + " " + randomExcuse(when) + "." 
 };
